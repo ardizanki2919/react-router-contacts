37e08c951a80ef73f57bce6bba114f8c4120d2fa
ardizanki2919 <setiawanardi2024@gmail.com>
2025-11-01 23:30:27 +0700

    manage the history stack

--- app/routes/contact.tsx
+++ app/routes/contact.tsx
@@ -1,8 +1,18 @@
-import { Form } from "react-router";
+import { Form, useFetcher } from "react-router";
 import type { ContactRecord } from "../data";
 
-import { getContact } from "../data";
+import { getContact, updateContact } from "../data";
 import type { Route } from "./+types/contact";
+
+export async function action({
+     params,
+     request,
+}: Route.ActionArgs) {
+    const formData = await request.formData();
+    return updateContact(params.contactId, {
+        favorite: formData.get("favorite") === "true",
+    });
+}
 
 export async function loader({ params }: Route.LoaderArgs) {
     const contact = await getContact(params.contactId);
@@ -79,21 +89,24 @@ function Favorite({
 }: {
     contact: Pick<ContactRecord, "favorite">;
 }) {
-    const favorite = contact.favorite;
+    const fetcher = useFetcher();
+    const favorite = fetcher.formData
+        ? fetcher.formData.get("favorite") === "true"
+        : contact.favorite;
     
     return (
-        <Form method="post">
+        <fetcher.Form method="post">
             <button
-            aria-label={
-                favorite
-                ? "Remove from favorites"
-                : "Add to favorites"
-            }
-            name="favorite"
-            value={favorite ? "false" : "true"}
+                aria-label={
+                    favorite
+                        ? "Remove from favorites"
+                        : "Add to favorites"
+                }
+                name="favorite"
+                value={favorite ? "false" : "true"}
             >
                 {favorite ? "★" : "☆"}
             </button>
-        </Form>
+        </fetcher.Form>
     );
 }
