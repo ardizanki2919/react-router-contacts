0c5061fc7d469185e7c934758ce96387234d5107
ardizanki2919 <setiawanardi2024@gmail.com>
2025-11-01 21:52:20 +0700

    add the about page UI

--- app/root.tsx
+++ app/root.tsx
@@ -1,78 +1,12 @@
 import {
-  Form,
-  Link,
   Outlet,
   Scripts,
   ScrollRestoration,
   isRouteErrorResponse,
 } from "react-router";
-import type { Route } from "./+types/root";
 
 import appStylesHref from "./app.css?url";
-
-import { getContacts } from "./data";
-import type { ContactRecord } from "./data";
-
-export async function clientLoader() {
-  const contacts = await getContacts();
-  return { contacts };
-}
-
-export default function App({ loaderData }: Route.ComponentProps) {
-  const { contacts } = loaderData;
-
-  return (
-    <>
-      <div id="sidebar">
-        <h1>Router Contacts</h1>
-        <div>
-          <Form id="search-form" role="search">
-            <input
-              aria-label="Search contacts"
-              id="q"
-              name="q"
-              placeholder="Search"
-              type="search"
-            />
-            <div aria-hidden hidden={true} id="search-spinner" />
-          </Form>
-          <Form method="post">
-            <button type="submit">New</button>
-          </Form>
-        </div>
-        <nav>
-          {contacts.length ? (
-            <ul>
-              {contacts.map((contact: ContactRecord) => (
-                <li key={contact.id}>
-                  <Link to={`contacts/${contact.id}`}>
-                    {contact.first || contact.last ? (
-                      <>
-                        {contact.first} {contact.last}
-                      </>
-                    ) : (
-                      <i>No Name</i>
-                    )}
-                    {contact.favorite ? (
-                      <span>★</span>
-                    ) : null}
-                  </Link>
-                </li>
-              ))}
-            </ul>
-          ) : (
-            <p>
-              <i>No Contacts</i>
-            </p>
-          )}
-        </nav>
-      </div>
-      <div id="detail">
-        <Outlet />
-      </div>
-    </>
-  );
-}
+import type { Route } from "./+types/root";
 
 // The Layout component is a special export for the root route.
 // It acts as your document's "app shell" for all route components, HydrateFallback, and ErrorBoundary
@@ -132,4 +66,8 @@ export function HydrateFallback() {
       <p>Loading, please wait...</p>
     </div>
   )
+}
+
+export default function App() {
+  return <Outlet />
 }
---
+++ app/routes/home.tsx
@@ -0,0 +1,13 @@
+export default function Home() {
+    return (
+        <p id="index-page">
+            This is a demo for React Router.
+            <br />
+            Check out{" "}
+            <a href="https://reactrouter.com/" target="_black">
+                the docs at reactrouter.com
+            </a>
+            .
+        </p>
+    );
+}
